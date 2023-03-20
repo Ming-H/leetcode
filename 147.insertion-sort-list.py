9cfcb915a9e56ebe9dea8ea16145f0af6feaa481
Ming-H <1518246548@qq.com>
2023-03-20 08:33:13 +0800

    update

--- 147.insertion-sort-list.py
+++ 147.insertion-sort-list.py
@@ -15,22 +15,25 @@ class Solution:
         """
         !!!!!!!!!!!!!!!!!!!!!!!!!!!!
         """
-        p = dummy = ListNode(0)
-        cur = dummy.next = head
-        while cur and cur.next:
-            val = cur.next.val
-            if cur.val < val:
-                cur = cur.next
-                continue
-            if p.next.val > val:
-                p = dummy
-            while p.next.val < val:
-                p = p.next
-            new = cur.next
-            cur.next = new.next
-            new.next = p.next
-            p.next = new
+        if not head or not head.next:
+            return head
+        dummy = ListNode(0)
+        dummy.next = head
+        curr = head.next
+        last_sorted = head
+        while curr:
+            if curr.val >= last_sorted.val:
+                last_sorted = last_sorted.next
+            else:
+                prev = dummy
+                while prev.next.val < curr.val:
+                    prev = prev.next
+                last_sorted.next = curr.next
+                curr.next = prev.next
+                prev.next = curr
+            curr = last_sorted.next
         return dummy.next
+
 
 
 
