8d9bf4017f5434badae355abcb36f181f8c560c8
Ming-H <1518246548@qq.com>
2023-03-21 09:46:46 +0800

    update

--- 204.count-primes.py
+++ 204.count-primes.py
@@ -1,7 +1,7 @@
 #
 # @lc app=leetcode id=204 lang=python3
 #
-# [204] Count Primes
+# [204] Count dp
 #
 
 # @lc code=start
@@ -12,14 +12,13 @@ class Solution:
         倍数全部标记出来，然后到下一个质数3，标记其所有倍数，
         一次类推，直到根号n，此时数组中未被标记的数字就是质数。
         """
-        if n <= 2:
+        if n < 2:
             return 0
         dp = [True] * n
         dp[0] = dp[1] = False
-        for i in range(2, n):
+        for i in range(2, int(n**0.5)+1):
             if dp[i]:
-                for j in range(i*i, n, i):
-                    dp[j] = False
+                dp[i*i:n:i] = [False] * len(dp[i*i:n:i])
         return sum(dp)
 
 
