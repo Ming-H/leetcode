09cf59324df09cba538b6dc2203b8985f7cf0e1a
Ming-H <1518246548@qq.com>
2023-05-19 20:51:04 +0800

    update

--- 78.subsets.py
+++ 78.subsets.py
@@ -11,14 +11,14 @@ class Solution:
         """
         res = []
         nums.sort()
-        def dfs(nums, index, path, res):
-            if path not in res:
-                res.append(path)
-            for i in range(index, len(nums)):
-                dfs(nums, i+1, path + [nums[i]], res)
-        dfs(nums, 0, [], res)
+        self.dfs(nums, 0, [], res)
         return res
-        
+
+    def dfs(self, nums, index, path, res):
+        if path not in res:
+            res.append(path)
+        for i in range(index, len(nums)):
+            self.dfs(nums, i+1, path + [nums[i]], res)
 
 
 
