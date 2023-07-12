5efa8c6977b0b194cc3567712a3f6be10c677bca
Ming-H <1518246548@qq.com>
2023-07-12 08:52:01 +0800

    update

--- 495.teemo-attacking.py
+++ 495.teemo-attacking.py
@@ -8,14 +8,11 @@
 class Solution:
     def findPoisonedDuration(self, nums: List[int], duration: int) -> int:
         if not nums:
-            return 0 
+            return 0
         res = 0
-        for i in range(len(nums)-1):
-            res += min(nums[i+1]-nums[i], duration)
-        return res + duration 
-
-
+        for i in range(1, len(nums)):
+            res += min(nums[i] - nums[i - 1], duration)
+        return res + duration
 
 
 # @lc code=end
-
--- 496.next-greater-element-i.py
+++ 496.next-greater-element-i.py
@@ -7,20 +7,13 @@
 # @lc code=start
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        stack, res  = [], []
+        stack = []
         d = {}
         for item in nums2:
-            while stack and item>stack[-1]:
-                d[stack.pop()] = item 
+            while stack and item > stack[-1]:
+                d[stack.pop()] = item
             stack.append(item)
-        for item in nums1:
-            if item in d:
-                res.append(d[item])
-            else:
-                res.append(-1)
-        return res 
-
+        return [d.get(item, -1) for item in nums1]
 
 
 # @lc code=end
-
