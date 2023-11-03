42b4e2a8b08bfb0eb002388a287a6243ef792835
Ming-H <1518246548@qq.com>
2023-11-03 09:18:34 +0800

    update 1103

--- 318.maximum-product-of-word-lengths.py
+++ 318.maximum-product-of-word-lengths.py
@@ -7,11 +7,16 @@
 # @lc code=start
 class Solution:
     def maxProduct(self, words: List[str]) -> int:
+        """
+        Time Complexity : O(n*(N+n)), where n is the length of words 
+        and N is the average length of words
+        Space Complexity : O(n)
+        """
         def check(chars1, chars2):
             for a, b in zip(chars1, chars2):
                 if a and b:
                     return True
-            return False 
+            return False
         chars, res = [[False]*26 for i in range(len(words))], 0
         for i, item in enumerate(words):
             for ch in item:
@@ -19,10 +24,7 @@ class Solution:
             for j in range(i):
                 if not check(chars[i], chars[j]):
                     res = max(res, len(words[i])*len(words[j]))
-        return res 
-
-
+        return res
 
 
 # @lc code=end
-
--- 367.valid-perfect-square.py
+++ 367.valid-perfect-square.py
@@ -7,6 +7,13 @@
 # @lc code=start
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
+        if num < 0:
+            return False
+        x, i = 0, 1
+        while x < num:
+            x += i
+            i += 2
+        return x == num
         
 # @lc code=end
 
