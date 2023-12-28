087e3ed76698d4d33ad2071aa7a5dcb46dd1bdaf
Ming-H <1518246548@qq.com>
2023-12-28 09:17:33 +0800

    1228

--- 215.kth-largest-element-in-an-array.py
+++ 215.kth-largest-element-in-an-array.py
@@ -16,6 +16,7 @@ class Solution:
     如果大于 k-1，说明要求的数字在左半部分，更新右边界，再求
     新的中枢点位置；反之则更新右半部分，求中枢点的位置
     """
+
     def findKthLargest(self, nums: List[int], k: int) -> int:
         left = 0
         right = len(nums)-1
@@ -27,23 +28,19 @@ class Solution:
                 right = pos - 1
             else:
                 left = pos + 1
-        
+
     def partition(self, nums, left,  right):
         pivot = nums[left]
         l = left + 1
         r = right
-        while l<=r:
+        while l <= r:
             if nums[l] < pivot < nums[r]:
                 nums[l], nums[r] = nums[r], nums[l]
                 l += 1
-                r -= 1            
+                r -= 1
             if nums[l] >= pivot:
                 l += 1
             if nums[r] <= pivot:
                 r -= 1
         nums[left], nums[r] = nums[r], nums[left]
         return r
-
-
-
-
