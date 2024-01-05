a903adaec1f0f609f176299d25ee068e3803543d
Ming-H <1518246548@qq.com>
2024-01-05 09:26:58 +0800

    update

--- 70.climbing-stairs.py
+++ 70.climbing-stairs.py
@@ -5,12 +5,9 @@
 #
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n==0:
+        if n <= 1:
             return 1
         a, b = 1, 1
         for _ in range(2, n+1):
             a, b = b, a+b
         return b
-
-
-        
